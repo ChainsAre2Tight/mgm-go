cf1252bd0d3a44c471357b3898ff7c53a527dee3
dmitry <dmitry@abobotron>
2025-05-14 16:50:50 +0300

    outlines encryption workflow

--- pkg/encryption/encrypt.go
+++ pkg/encryption/encrypt.go
@@ -28,4 +28,30 @@ func (e *encryptor) Encrypt(
 	err error,
 ) {
 	panic("unimplemented")
+
+	// schedule keys
+
+	// get nonce
+
+	// create MAC
+
+	// step 1: sign associated data
+	// use nonce and convert 1||nonce to []byte
+	// encrypt this nonce
+	// increment result h times, encrypting each step in a goroutine
+	// MULTIPLY encryption result with a corresponding accosiated data block
+	// convert result to bitString and XOR it with cumulative result
+
+	// step 2: encrypt plaintext and sign it (in parallel)
+	// use nonce and convert 0||nonce to []byte
+	// encrypt this nonce
+	// increment result Q times, encrypting each step in a goroutine
+	// XOR encrypted result with a corresponding plaintext block
+	// convert []byte to bitString
+	// increment MAC counter and multiply new encryption result with encrypted MAC counter result
+	// XOR it with cumulative mac result
+
+	// step 3: multiply and xor len(a) || len(c)
+
+	// step 4: Encrypt cumulative result, taking first S bits from it
 }
